f26b235ed728fda17feb9ece7f2cbb64b84e0405
eliaslua001 <157940043+eliaslua001@users.noreply.github.com>
2024-03-09 23:51:11 +0800

    astro distancemeter markerline

--- script.js
+++ script.js
@@ -358,13 +358,11 @@ window.addEventListener('scroll', function () {
     const neptuneOffset = document.getElementById('neptune').getBoundingClientRect().top;
     if (neptuneOffset < 0 || neptuneOffset > window.innerHeight) {
       document.querySelector('.astronaut').style.display = 'none';
-    } else {
-      document.querySelector('.astronaut').style.display = 'block';
-    }
-
-    if (neptuneOffset > window.innerHeight) {
       document.querySelector('.distance-meter').style.display = 'none';
       document.querySelector('.marker-line').style.display = 'none';
+    } else {
+      document.querySelector('.astronaut').style.display = 'block';
+      document.querySelector('.distance-meter').style.display = 'block';
     }
   }, 50); // Adjust the debounce delay as needed
 });
