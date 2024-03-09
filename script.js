726930e5fca517cd07ee068843d31b4524127e35
eliaslua001 <157940043+eliaslua001@users.noreply.github.com>
2024-03-09 14:25:08 +0800

    POSTER

--- script.js
+++ script.js
@@ -252,8 +252,6 @@ celestialBodies.forEach(body => {
   modelViewer.style.height = size + 'px'; // Set the desired height for the model
   modelViewer.style.top = distance + 'px'; // Set the distance from the top
   modelViewer.style.left = 'calc(50% - ' + (size / 2) + 'px)'; // Adjust the left position to center the body
-  modelViewer.style.background = `url(${body.poster})`; // Set the background image to the poster image
-  modelViewer.style.backgroundSize = 'cover'; // Ensure the background image covers the entire element
   document.querySelector('.container').appendChild(modelViewer);
 
   // Function to set the glow effect for each celestial body
